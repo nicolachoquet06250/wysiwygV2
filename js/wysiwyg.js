865f05b7f3401151d0410e58aaa43c3ed5cc87b8
nicolas.choquet <nicolas.choquet@lagardere-active.com>
2019-04-10 17:00:19 +0200

    create dynamic javascript wysiwyg

--- js/wysiwyg.js
+++ js/wysiwyg.js
@@ -23,6 +23,212 @@ function commande(nom, argument) {
     }
 }
 
-function resultat() {
-    document.getElementById("resultat").value = document.getElementById("editeur").innerHTML;
+function resultat(elem, editor) {
+    elem.value = editor.innerHTML;
 }
+
+window.addEventListener('load', () => {
+    let editors = document.querySelectorAll('wysiwyg');
+    editors.forEach(editor => {
+        let commands = document.createElement('div');
+        commands.classList.add('commands');
+
+        let b = document.createElement('input');
+        b.setAttribute('type', 'button');
+        b.value = 'G';
+        b.style.fontWeight = 'bold';
+        b.addEventListener('click', () => {
+            commande('bold');
+        });
+
+        commands.append(b);
+
+        let i = document.createElement('input');
+        i.setAttribute('type', 'button');
+        i.value = 'I';
+        i.style.fontStyle = 'italic';
+        i.addEventListener('click', () => {
+            commande('italic');
+        });
+
+        commands.append(i);
+
+        let u = document.createElement('input');
+        u.setAttribute('type', 'button');
+        u.value = 'U';
+        u.style.textDecoration = 'underline';
+        u.addEventListener('click', () => {
+            commande('underline');
+        });
+
+        commands.append(u);
+
+        let alignements = document.createElement('select');
+        alignements.addEventListener('change', () => {
+            commande(alignements.value);
+            alignements.selectedIndex = 0;
+        });
+        let first_option_align = document.createElement('option');
+        first_option_align.value = '';
+        first_option_align.innerHTML = 'Allignements';
+        alignements.append(first_option_align);
+
+        let left = document.createElement('option');
+        left.value = 'justifyLeft';
+        left.innerHTML = 'Gauche';
+        alignements.append(left);
+
+        let center = document.createElement('option');
+        center.value = 'justifyCenter';
+        center.innerHTML = 'Centré';
+        alignements.append(center);
+
+        let justify = document.createElement('option');
+        justify.value = 'justifyFull';
+        justify.innerHTML = 'Justifié';
+        alignements.append(justify);
+
+        let right = document.createElement('option');
+        right.value = 'justifyRight';
+        right.innerHTML = 'Droite';
+        alignements.append(right);
+
+        commands.append(alignements);
+
+        let links = document.createElement('input');
+        links.setAttribute('type', 'button');
+        links.value = 'Liens';
+        links.addEventListener('click', () => {
+            commande('createLink');
+        });
+
+        commands.append(links);
+
+        let images = document.createElement('input');
+        images.setAttribute('type', 'button');
+        images.value = 'Image';
+        images.addEventListener('click', () => {
+            commande('insertImage');
+        });
+
+        commands.append(images);
+
+        let youtube = document.createElement('input');
+        youtube.setAttribute('type', 'button');
+        youtube.value = 'Vidéo Youtube';
+        youtube.addEventListener('click', () => {
+            insert_youtube_video();
+        });
+
+        commands.append(youtube);
+
+        let titles = document.createElement('select');
+        titles.addEventListener('change', () => {
+            commande('heading', titles.value);
+            titles.selectedIndex = 0;
+        });
+        let first_option_titles = document.createElement('option');
+        first_option_titles.value = '';
+        first_option_titles.innerHTML = 'Titres';
+        titles.append(first_option_titles);
+
+        let h1 = document.createElement('option');
+        h1.value = 'h1';
+        h1.innerHTML = 'Titre 1';
+        titles.append(h1);
+
+        let h2 = document.createElement('option');
+        h2.value = 'h2';
+        h2.innerHTML = 'Titre 2';
+        titles.append(h2);
+
+        let h3 = document.createElement('option');
+        h3.value = 'h3';
+        h3.innerHTML = 'Titre 3';
+        titles.append(h3);
+
+        let h4 = document.createElement('option');
+        h4.value = 'h4';
+        h4.innerHTML = 'Titre 4';
+        titles.append(h4);
+
+        let h5 = document.createElement('option');
+        h5.value = 'h5';
+        h5.innerHTML = 'Titre 5';
+        titles.append(h5);
+
+        let h6 = document.createElement('option');
+        h6.value = 'h6';
+        h6.innerHTML = 'Titre 6';
+        titles.append(h6);
+
+        commands.append(titles);
+
+        editor.append(commands);
+
+        let wys = document.createElement('div');
+        wys.classList.add('editor');
+        wys.setAttribute('contentEditable', 'true');
+
+        editor.append(wys);
+
+        wys.addEventListener('focus', () => {
+            wys.addEventListener('keydown', evt => {
+                if(evt.ctrlKey && (evt.key === 'l' || evt.key === 'L')) {
+                    evt.preventDefault();
+                    commande('justifyLeft');
+                }
+                else if(evt.ctrlKey && (evt.key === 'r' || evt.key === 'R')) {
+                    evt.preventDefault();
+                    commande('justifyRight');
+                }
+                else if(evt.ctrlKey && (evt.key === 'j' || evt.key === 'J')) {
+                    commande('justifyFull');
+                }
+                else if(evt.ctrlKey && (evt.key === 'c' || evt.key === 'C')) {
+                    commande('justifyCenter');
+                }
+                else if(evt.ctrlKey && (evt.key === 'b' || evt.key === 'B')) {
+                    evt.preventDefault();
+                    commande('bold');
+                }
+                else if(evt.ctrlKey && (evt.key === 'i' || evt.key === 'I')) {
+                    evt.preventDefault();
+                    commande('italic');
+                }
+                else if(evt.ctrlKey && (evt.key === 'u' || evt.key === 'U')) {
+                    evt.preventDefault();
+                    commande('underline');
+                }
+            });
+            wys.addEventListener('keyup', evt => {
+                let content = wys.innerHTML;
+                if(content.indexOf(' :D ') !== -1) {
+                    // console.log(content_text.substr(content_text.length-4, 3));
+                    content = content.replace(' :D', ' <img src="./images/smiles/smile.png" class="smile" alt=":D" /> ');
+                    wys.innerHTML = content;
+                }
+            });
+        });
+
+        // debug
+        if(editor.hasAttribute('debug')) {
+            let result_button_container = document.createElement('div');
+            let result_button = document.createElement('input');
+            result_button.value = 'Obtenir le HTML';
+            result_button.setAttribute('type', 'button');
+
+            result_button_container.append(result_button);
+            editor.append(result_button_container);
+
+            let result = document.createElement('textarea');
+            result.classList.add('html_result');
+
+            result_button.addEventListener('click', () => {
+                resultat(result, wys);
+            });
+
+            editor.append(result);
+        }
+    });
+});
